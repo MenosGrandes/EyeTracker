b1c603a9f264342b5d0f2dc3bb789c734c607cc6
MenosGrandes <kaczor201@o2.pl>
2018-03-31 15:17:55 +0200

    Added treshold as param

--- Hough/houghCircular.hpp
+++ Hough/houghCircular.hpp
@@ -5,7 +5,7 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-
+//#include "../precompiled/CompileTimeAngle.hpp"
 
 
 #include <iostream>
@@ -26,20 +26,21 @@ public:
         delete function;
     };
 
-    std::vector<cv::Point3f > calculate(cv::Mat &mat,int min,int max)
+    std::vector<cv::Point3f > calculate(cv::Mat &mat,int min,int max,int treshold) const
     {
         if(max>mat.rows)
         {
-            max = mat.rows-3;
+            max = std::min(mat.cols ,mat.rows)/2;
         }
-        std::cout<<"min : "<<min <<"max" << max<<std::endl;
+//        std::cout<<"min : "<<min <<"max" << max<<std::endl;
         std::vector<cv::Point3f> circles(100);
-        circles.reserve(10000);
-        std::vector<cv::Mat> hough_spaces(max*2);
-        for(int i=min; i<max*2; ++i)
+        circles.reserve(100);
+        std::vector<cv::Mat> hough_spaces(max);
+        for(int i=0; i<max; ++i)
         {
             hough_spaces[i] = cv::Mat::zeros(mat.rows,mat.cols,mat.type());
         }
+//	std::cout<<mat.rows<<" "<<mat.cols<<std::endl;
         // const int sz[3] = {mat.rows,mat.cols,max-min};
         //cv::Mat L(3,sz, CV_8UC(1), cv::Scalar::all(0));
         for(int x=0; x<mat.rows; ++x)
@@ -48,18 +49,21 @@ public:
             for(int y=0; y<mat.cols; ++y)
             {
 
-                if(mat.at<uchar>(x,y) == 255) {
+                if(mat.at<uchar>(x,y) == 255)
+                {
                     for(int radius=min; radius<max; ++radius)
                     {
-                        std::cout<<radius<<"radius"<<std::endl;
                         for(int theta = 0; theta<360; ++theta)
                         {
-                            const float a= x - radius * cos(theta * PI_180);
-                            const float b=y- radius *sin(theta * PI_180);
-                            //if(a <0 || a >= mat.rows || b < 0 || b >= mat.cols){
-			    hough_spaces[radius].at<uchar>(a,b)++;
-			   // }
-                            //L.at<uchar>(a,b,radius)+=1;
+                            const int a = floor(x - radius * cos(theta * PI_180));//radius * cos can be precomputed and used multiple times
+                            const int b = floor(y - radius * sin(theta * PI_180));
+                            if(a <0 || a >= hough_spaces[radius].rows ||
+                             b < 0 || b >= hough_spaces[radius].cols)
+                            {
+                                break;
+                            }
+                            hough_spaces[radius].at<uchar>(a,b)++;
+
                         }
                     }
                 }
@@ -67,115 +71,21 @@ public:
 
 
         }
-/*
-        const int treshold = 20;
-        for(int radius=(max - min); radius<max; ++radius)
+        for(int radius=min; radius<max; ++radius)
         {
-            for(int x=mat.rows; x<mat.rows; ++x)
-            {
-                for(int y=mat.cols; y<mat.rows; ++y)
-                {
-                    if(hough_spaces[radius].at<uchar>(x,y) >treshold)
-                    {
-                        std::cout<<"CIRCLE"<<std::endl;
-                        circles.emplace_back(cv::Point3f(x,y,radius));
-                    }
-                }
 
+            double min_f,max_f;
+            cv::Point min_loc,max_loc;
+            cv::minMaxLoc(hough_spaces[radius],&min_f,&max_f,&min_loc,&max_loc);
+            if(max_f>treshold)
+            {
+                circles.emplace_back(cv::Point3f(max_loc.x,max_loc.y,radius));
             }
         }
-	*/
-        //cv::Mat current_space = L.subm
-        imshow("i",hough_spaces[4]);
-
-        /*
-            for(int radius = min; radius< max; ++radius )
-            {
-                cv::Mat &hough_space = hough_spaces[radius-min];
-                hough_space.create(mat.rows,mat.cols,mat.type());
-
-
-                for(int x=0; x<mat.cols; ++x)
-                {
-
-                    for(int y=0; y<mat.rows; ++y)
-                    {
-                        if(mat.at<uchar>(x,y) == 255)
-                        {
-                            //accum_circle(hough_space,cv::Point2i(y,x),radius);
-
-        	    }
-                    }
-                }
-            imshow("i",hough_space);//hough_space);
-
-            const int treshold = 5 *radius;
-                for(int x=0; x<hough_space.cols; ++x)
-                {
-
-                    for(int y=0; y<hough_space.rows; ++y)
-                    {
-                        if(hough_space.at<uchar>(x,y) > treshold)
-                        {
-
-                            circles.emplace_back(cv::Point3f(x,y,radius));
-                        }
-                    }
-                }
-            }*/
         circles.shrink_to_fit();
         return circles;
 
     }
-private :
-
-    void accum_circle(cv::Mat & image,const cv::Point2i &position,unsigned int radius)//image &image, const cv::point2i &position, unsigned int radius)
-    {
-        int f = 1 - radius;
-        int ddf_x = 1;
-        int ddf_y = -2 * radius;
-        int x = 0;
-        int y = radius;
-
-        accum_pixel(image, cv::Point2i(position.x, position.y + radius));
-        accum_pixel(image, cv::Point2i(position.x, position.y - radius));
-        accum_pixel(image, cv::Point2i(position.x + radius, position.y));
-        accum_pixel(image, cv::Point2i(position.x - radius, position.y));
-
-        while(x < y)
-        {
-            if(f >= 0)
-            {
-                y--;
-                ddf_y += 2;
-                f += ddf_y;
-            }
-
-            x++;
-            ddf_x += 2;
-            f += ddf_x;
-
-            accum_pixel(image, cv::Point2i(position.x + x, position.y + y));
-            accum_pixel(image, cv::Point2i(position.x - x, position.y + y));
-            accum_pixel(image, cv::Point2i(position.x + x, position.y - y));
-            accum_pixel(image, cv::Point2i(position.x - x, position.y - y));
-            accum_pixel(image, cv::Point2i(position.x + y, position.y + x));
-            accum_pixel(image, cv::Point2i(position.x - y, position.y + x));
-            accum_pixel(image, cv::Point2i(position.x + y, position.y - x));
-            accum_pixel(image, cv::Point2i(position.x - y, position.y - x));
-        }
-    }
-    void accum_pixel(cv::Mat &image, const cv::Point2i &position)
-    {
-        /* bounds checking */
-        if(position.x < 0 || position.x >= image.cols ||
-                position.y < 0 || position.y >= image.rows)
-        {
-            return;
-        }
-        image.at<uchar>(position.x,position.y)++;
-    }
-
 
 };
 
--- main.cpp
+++ main.cpp
@@ -10,63 +10,50 @@ int main(int, char**)
 {
 
     Clock clock;
-Hough<int> hough;
- 
-/*
+    Hough<int> hough;
 
-VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-    {
-        std::cout<<"NO CAMERA"<<std::endl;
-        return -1;
-    }
-  */
+    /*
+
+    VideoCapture cap(0); // open the default camera
+        if(!cap.isOpened())  // check if we succeeded
+        {
+            std::cout<<"NO CAMERA"<<std::endl;
+            return -1;
+        }
+      */
     Mat gray;
     namedWindow("edges",1);
 
- 
- 
- 
+
+
+
     //for(;;)
     {
 
-       Mat frame = imread("2.jpg",cv::IMREAD_COLOR)	;
-//	Mat frame;      
- // cap >> frame; // get a new frame from camera
-       
-	 cvtColor(frame, gray, COLOR_BGR2GRAY);
-	Mat hough_accum = frame.clone();//(frame.size,frame.type());
+        Mat frame = imread("1.jpg",cv::IMREAD_COLOR)	;
+//	Mat frame;
+// cap >> frame; // get a new frame from camera
 
- 	 GaussianBlur(gray,gray, Size(9,9), 0, 0);
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        Mat hough_accum = frame.clone();//(frame.size,frame.type());
+
+        GaussianBlur(gray,gray, Size(9,9), 0, 0);
         Canny(gray,gray,100,150,3);
-   //     std::vector<Vec3f> circles;
-/*        HoughCircles(gray,circles,HOUGH_GRADIENT,2,gray.rows/4,200,100);
-        for( size_t i = 0; i < circles.size(); i++ )
+        const double start = clock.getTime();//CLOCK();
+        const int treshold = 20;
+        const std::vector<cv::Point3f> circles = hough.calculate(gray,40,100,treshold);
+        for(auto circle : circles)
         {
-            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-            int radius = cvRound(circles[i][2]);
-// draw the circle center
-            circle( frame, center, 3, Scalar(0,255,0), -1, 8, 0 );
-// draw the circle outline
-            circle( frame, center, radius, Scalar(0,0,255), 3, 8, 0 );
-*/
-        
-      const double start = clock.getTime();//CLOCK();  
-      std::vector<cv::Point3f> circles = hough.calculate(gray,1,5);
-	for(auto circle : circles)
-	{
-		cv::circle(hough_accum,cv::Point2f(circle.x,circle.y),circle.z,cv::Scalar(255,255,0),1,2,0);
-	}
+            cv::circle(hough_accum,cv::Point2f(circle.x,circle.y),circle.z,cv::Scalar(255,0,0),1,2,0);
+        }
 
         const double dur = clock.getTime()- start;
         //putText(gray,std::to_string(dur).c_str(),cv::Point(100,100),cv::FONT_HERSHEY_PLAIN,2,cv::Scalar(0,0,0,255));
-	std::cout<<std::to_string(dur).c_str()<<std::endl;
-	//Canny(edges, edges, 0, 30, 3);
+        std::cout<<std::to_string(dur).c_str()<<std::endl;
         imshow("edges", gray);
-	imshow("hough_space",hough_accum);
+        imshow("hough_space",hough_accum);
         waitKey(0); //break;
     }
-    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
 
